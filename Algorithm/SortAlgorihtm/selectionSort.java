2df292ae8abada966c767a3ff5c32ff64941b758
TheRainbow5 <3021809270@qq.com>
2023-09-05 22:11:10 +0800

    修改选择排序

--- Algorithm/SortAlgorihtm/selectionSort.java
+++ Algorithm/SortAlgorihtm/selectionSort.java
@@ -18,14 +18,6 @@ public class selectionSort {
 
     /**
      * 选择排序
-     * 排序数组：（2 4 3 1 5 2）
-     * 1. （**2** 4 3 1 5 2）：2依次和`4 3 1 5 2`比较，$if(2>other) ⇒ index=minIndex$，比较完后，交换元素位置。
-     * 2. （1 **4** 3 2 5 2）：4依次和`3 2 5 2`比较，同理得到最小元素的index，比较完后，交换元素位置。
-     * 3. （1 2 **3** 4 5 2）：3依次和`4 5 2`比较，同理，交换元素位置。
-     * 4. （1 2 2 **4** 5 3）
-     * 5. （1 2 2 3 **5** 4）
-     * 6. （1 2 2 3 ****4 5）
-     * T(n)=O(n^2)
      */
     private static void selectionSorted(int[] array) {
         int length = array.length;
